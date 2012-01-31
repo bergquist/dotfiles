36d2ee7d98e71286d7a1432482bfa026bc5702ed
Carl Bergquist <carl.bergquist@gmail.com>
2012-01-31 21:28:15 +0100

    remove js unpackar and improved python unpacker

--- upack.py
+++ upack.py
@@ -1,18 +1,19 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 import os
 import sys
 import commands
 
 movieDir = '/movies/'
 currentDir = os.getcwd()
-if sys.argv[0] != None:
-  movieDir = sys.argv[0]
+if len(sys.argv) > 1 and sys.argv[1] != None:
+  movieDir = sys.argv[1]
 
 for child in os.listdir(currentDir):
   if os.path.isdir(child):
     subchild = os.path.join(currentDir, child)
     for rarfile in os.listdir(subchild):
       if rarfile.find('.rar') > 0:
-        cmd = 'unrar x -inul -y ' + os.path.join(subchild, rarfile) + ' ' + movieDir
-        commands.getstatusoutput(cmd)
+        cmd = 'unrar e -kb ' + os.path.join(subchild, rarfile) + ' ' + movieDir
+        print cmd
+#        commands.getstatusoutput(cmd)
     
