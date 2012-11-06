339fdb784af6da5922a0111d043ef4c4ce2f821a
Carl Bergquist <carl.bergquist@gmail.com>
2012-11-06 23:00:28 +0100

    converted tabs to spaces

--- bin/upack.py
+++ bin/upack.py
@@ -4,26 +4,26 @@ import sys
 import commands
 
 def subFolders():
-	current_dir = os.getcwd();
-	for child in os.listdir(current_dir):
-		if os.path.isdir(child):
-			yield os.path.join(current_dir, child)
+  current_dir = os.getcwd();
+  for child in os.listdir(current_dir):
+    if os.path.isdir(child):
+      yield os.path.join(current_dir, child)
 
 def getDestination():
-	if len(sys.argv) <= 1 or sys.argv[1]:
-		return '/movies/'
+  if len(sys.argv) <= 1 or sys.argv[1]:
+    return '/movies/'
 
-	return sys.argv[1]
+  return sys.argv[1]
 
 def getFirstrarefile(subchild):
-	for rarfile in os.listdir(subchild):
-		if rarfile.find('.rar') > 0:
-			return os.path.join(subchild, rarfile)
+  for rarfile in os.listdir(subchild):
+    if rarfile.find('.rar') > 0:
+      return os.path.join(subchild, rarfile)
 
 def extract(rarfile):
-	cmd = 'unrar e -kb ' + rarfile + ' ' + getDestination()
-	print 'extracting ' + rarfile.split('/')[-1]
-	commands.getstatusoutput(cmd)
+  cmd = 'unrar e -kb ' + rarfile + ' ' + getDestination()
+  print 'extracting ' + rarfile.split('/')[-1]
+  commands.getstatusoutput(cmd)
 
 for folder in subFolders():
-	extract(getFirstrarefile(folder))
+  extract(getFirstrarefile(folder))
