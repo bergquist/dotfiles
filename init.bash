ac518f4292820509603266053fee0ad1699b1c1b
Carl Bergquist <carl.bergquist@gmail.com>
2014-01-21 12:10:47 +0100

    remove commented "code"

--- init.bash
+++ init.bash
@@ -2,12 +2,6 @@ source "${DOTFILES}/prompt/colors.theme.bash"
 source "${DOTFILES}/prompt/base.theme.bash"
 source "${DOTFILES}/prompt/sexy.theme.bash"
 
-#for source in `find $DOTFILES -maxdepth 2 -name \*.bash`
-#  do
-#    dest="$HOME/.`basename \"${source%.*}\"`"
-#  done
-#}
-
 LIB="${DOTFILES}/system/*.bash"
 for config_file in $LIB
 do
