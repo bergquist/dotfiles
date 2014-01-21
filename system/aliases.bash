363490e08afcd54691c981f65d00ee00f3a22dbb
Carl Bergquist <carl.bergquist@gmail.com>
2014-01-21 22:10:04 +0100

    add some cool alias. like sniff!

--- system/aliases.bash
+++ system/aliases.bash
@@ -11,3 +11,13 @@ fi
 
 #alias ls="ls -FHG"
 alias grep="grep --color=auto"
+
+# View HTTP traffic
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+# OS X has no `md5sum`, so use `md5` as a fallback
+command -v md5sum > /dev/null || alias md5sum="md5"
+
+# Lock the screen (when going AFK)
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
