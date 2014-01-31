d93bd41808b66cc9f2be3a8277f23c498556483a
bergquist <carl.bergquist@gmail.com>
2014-01-31 16:55:30 +0100

    feat: add npm global path!

--- system/path.bash
+++ system/path.bash
@@ -4,5 +4,6 @@ export DOTFILES=~/.dotfiles
 
 export PATH=/usr/bin:$PATH
 export PATH=~/.dotfiles/bin:$PATH
+export PATH=/usr/local/share/npm/bin:$PATH
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
