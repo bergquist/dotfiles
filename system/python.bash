fb48f76d7c70187d92652dbab13a5b2fb8cfa50f
bergquist <carl.bergquist@gmail.com>
2015-03-23 14:21:01 +0100

    add windows support for http command

--- system/python.bash
+++ system/python.bash
@@ -1,6 +1,8 @@
 if [ $(uname) = "Linux" ]
 then
-  alias http='python2 -m SimpleHTTPServer'
+	alias http='python2 -m SimpleHTTPServer $1'
+elif [[ $(uname) = "MINGW32_NT-6.2" ]]; then
+	alias http='/c/tools/python2/python.exe -m SimpleHTTPServer $1'
 else
-  alias http='python -m SimpleHTTPServer'
+	alias http='python -m SimpleHTTPServer $1'
 fi
