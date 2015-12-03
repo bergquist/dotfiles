5c4845af390c0bb0dffd05bbcfcd0a7558584561
carl bergquist <carl.bergquist@gmail.com>
2015-12-03 17:07:25 +0100

    add some basic

---
+++ apt-get.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+
+sudo apt-get update
+
+sudo apt-get install -y nautilus-dropbox
+sudo apt-get install -y vim
+sudo apt-get install -y sublime-text-installer
+
