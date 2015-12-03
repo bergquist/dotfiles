efd4d8e550bf226b6e320c3ef91e0be30e8fed6f
carl bergquist <carl.bergquist@gmail.com>
2015-12-03 11:16:57 +0100

    remove mac specific settings

--- system/scala.bash
+++ system/scala.bash
@@ -1,8 +1,5 @@
-export JAVA_HOME=$(/usr/libexec/java_home)
-
-export SCALA_HOME=/usr/local/Cellar/scala/2.11.1/libexec
 export JAVACMD=drip
 export DRIP_SHUTDOWN=30
 export SBT_OPTS="-XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:PermSize=128M -XX:MaxPermSize=512M"
 
-export PATH=/Users/yobo/dev/activator:$PATH
+export PATH=~/dev/activator:$PATH
