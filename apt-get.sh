d7621ff973dd7c7a13b39033355f3acdbcf504cf
bergquist <carl.bergquist@gmail.com>
2016-10-04 09:10:06 +0200

    update apt-get command

--- apt-get.sh
+++ apt-get.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-
 sudo apt-get update
 
-sudo apt-get install -y nautilus-dropbox
 sudo apt-get install -y vim
-sudo apt-get install -y sublime-text-installer
+sudo apt-get install -y tmux
 
