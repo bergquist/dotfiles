e2d7e70c5ec1f0e8dc01bbc856e04c57d4758ca0
bergquist <carl.bergquist@gmail.com>
2017-12-14 14:16:04 +0100

    format format

--- bin/install.sh
+++ bin/install.sh
@@ -705,6 +705,16 @@ install_protobuf() {
 	)
 }
 
+install_spotify() {
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+	sudo apt-get update
+
+	sudo apt-get install spotify-client
+}
+
 usage() {
 	echo -e "install.sh\n\tThis script installs my basic setup for a debian laptop\n"
 	echo "Usage:"
@@ -765,6 +775,8 @@ main() {
 		install_vim
 	elif [[ $cmd == "vscode" ]]; then
 		install_vscode
+	elif [[ $cmd == "spotify" ]]; then
+		install_spotify
 	elif [[ $cmd == "golang" ]]; then
 		install_golang "$2"
     elif [[ $cmd == "golang_devenv" ]]; then
